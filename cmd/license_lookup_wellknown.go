239448ff2996af12265d7706c08dd679e9ef0007
Stephan Eberle <stephaneberle9@gmail.com>
2026-03-17 17:49:22 +0100

    fix: add missing field comparison in well-known license lookup

--- cmd/license_lookup_wellknown.go
+++ cmd/license_lookup_wellknown.go
@@ -100,7 +100,7 @@ func lookupLicenseForWellknownMavenComponents(cdxComponent schema.CDXComponent) 
 		}
 	}
 	if cdxComponent.Group == "com.itemis" {
-		if cdxComponent.Name == "com-itemis-secure" || cdxComponent.Name == "com-itemis-secure-jvm" || "secure-calculation-jvm" {
+		if cdxComponent.Name == "com-itemis-secure" || cdxComponent.Name == "com-itemis-secure-jvm" || cdxComponent.Name == "secure-calculation-jvm" {
 			return licenseWithId("LicenseRef-itemis-Closed")
 		}
 	}
